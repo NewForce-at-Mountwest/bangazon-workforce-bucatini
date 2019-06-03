c49d1f58e69feac0ec53182cd95a0dba85dcacb5
Charles Belcher <belchercharles@yahoo.com>
2019-06-03 10:04:28 -0400

    Added models and API reference

---
+++ BangazonWorkforce/BangazonWorkforceMVC/BangazonWorkforceMVC/Models/PaymentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class PaymentType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long AcctNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public bool Archived { get; set; }
+    }
+}
---
+++ BangazonWorkforce/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+
+namespace BangazonAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public bool Archived { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int CustomerId { get; set; }
+        
+        public PaymentType PaymentType { get; set; }
+        public Customer Customer { get; set; }
+
+
+        public List<Product> OrderProducts { get; set; } = new List<Product>();
+        
+
+    }
+}
