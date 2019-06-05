cd110ad55649549e85100fc361982c9c043144e9
BatsGoScreechh <bird11@mctc.edu>
2019-06-05 11:43:56 -0400

    Created Department Details, View, Controller, and Repo. Created working code for corresponding ticket. Ready for PR

---
+++ BangazonWorkforce/BangazonWorkforceMVC/BangazonWorkforceMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWorkforceMVC.Models;
+using BangazonWorkforceMVC.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonWorkforceMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentController(IConfiguration config)
+        {
+            DepartmentRepository.SetConfig(config);
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int id)
+        {
+            Department department = DepartmentRepository.GetDepartmentDetails(id);
+            ViewData["Title"] = department.Name;
+            return View(department);
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BangazonWorkforce/BangazonWorkforceMVC/BangazonWorkforceMVC/Repositories/DepartmentRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Configuration;
+using BangazonWorkforceMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforceMVC.Repositories
+{
+    public class DepartmentRepository
+    {
+
+        private static IConfiguration _config;
+
+        public static void SetConfig(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        public static Department GetDepartmentDetails(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+SELECT Department.Id, Department.[Name] AS 'DeptName', Employee.Id, Employee.FirstName, Employee.LastName, Employee.DepartmentId, Department.Budget FROM Department LEFT JOIN Employee ON Department.Id = Employee.DepartmentId WHERE Department.Id = @id
+        ";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department departmentDisplayed = null;
+
+                    while (reader.Read())
+                    {
+                        Department department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("DeptName")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                            
+                        };   
+                        
+                        //If departmentDisplayed is null, make department departmentDisplayed
+                        if (departmentDisplayed == null)
+                        {
+                            departmentDisplayed = department;
+                        }
+
+                        //Checks to see if departmentDisplayed has employees in the EmployeesInDepartment list. If it does, build the employee object and add it
+                        if(departmentDisplayed.EmployeesInDepartment != null)
+                        {
+
+                                    Employee employee = new Employee
+                                    {
+                                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                        FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                        LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                        DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId"))
+                                    };
+                            departmentDisplayed.EmployeesInDepartment.Add(employee);
+
+                        }
+                    };
+                    reader.Close();
+                    return departmentDisplayed;
+
+                }
+            }
+        }
+    }
+}
